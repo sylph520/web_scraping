1b7e16963b7b44e37bc421275527c1f98171b46f
sylph520 <pallasjack@outlook.com>
2017-06-13 12:01:42 +0800

    first commit

--- changeip.py
+++ changeip.py
@@ -9,9 +9,9 @@ nic_configs = wmi.WMI().Win32_NetworkAdapterConfiguration(IPEnabled=True)
 nic = nic_configs[0]
 
 # IP address, subnetmask and gateway values should be unicode objects
-ip = u'192.168.0.11'
+ip = u'10.144.21.62'
 subnetmask = u'255.255.255.0'
-gateway = u'192.168.0.1'
+gateway = u'10.144.21.1'
 
 # Set IP address, subnetmask and default gateway
 # Note: EnableStatic() and SetGateways() methods require *lists* of values to be passed
